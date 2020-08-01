529297f11f7cc363f78720b978c399b274e5f8e4
Leon Segal <leonsegal1@gmail.com>
2020-08-01 11:39:44 +0100

    add basic ui

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const server = require("http").createServer(app);
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/", (req, res) => {
   res.status(200).send("Working");
