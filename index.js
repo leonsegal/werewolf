da7229c14b473b86a021b7655e7b05ff2ffc5237
Leon Segal <leonsegal1@gmail.com>
2020-08-01 11:26:27 +0100

    add basic server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const server = require("http").createServer(app);
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Working");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
