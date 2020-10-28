b5c96056ac653e88e0dc58b5b0d9727723861f8c
Leon Segal <leonsegal1@gmail.com>
2020-10-28 20:56:59 +0000

    add socket/chat prototype

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 
 const app = express();
+
 const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname + "/public")));
+
+io.on("connection", () => {
+  console.log("Some client connected");
+
+  io.on("chat", (message) => {
+    console.log("From client: ", message);
+  });
+});
 
 app.get("/", (req, res) => {
   res.status(200).send("Working");
